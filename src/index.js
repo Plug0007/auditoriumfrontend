bab39408403e1cf7309868bdb4059ec550f44d4e
Raelyaan plug <plug9999999@gmail.com>
2025-03-10 23:25:57 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 import './styles/App.css';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,3 +11,6 @@ root.render(
     <App />
   </BrowserRouter>
 );
+
+// Register the service worker for offline support & PWA features
+serviceWorkerRegistration.register();
