78f844450d0056fc5e9fd50242af3977795714e9
Raelyaan plug <plug9999999@gmail.com>
2025-03-11 11:51:43 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
-import Intro from './components/PwaIntro'; // or PwaIntro if you have one
+import Intro from './components/Intro';
+import Home from './components/Home';
 import Login from './components/Login';
 import AdminDashboard from './components/AdminDashboard';
 import FacultyDashboard from './components/FacultyDashboard';
@@ -13,30 +14,26 @@ function App() {
   const [alertMessage, setAlertMessage] = useState('');
   const [alertType, setAlertType] = useState('info');
 
-  const showToast = (msg, type = 'success') => {
+  const showAlert = (msg, type = 'success') => {
     setAlertMessage(msg);
     setAlertType(type);
   };
 
   return (
-    <div className="app-container">
+    <>
       <Header />
       {alertMessage && (
-        <AlertModal 
-          message={alertMessage} 
-          type={alertType} 
-          onClose={() => setAlertMessage('')} 
-        />
+        <AlertModal message={alertMessage} type={alertType} onClose={() => setAlertMessage('')} />
       )}
       <Routes>
-        <Route path="/" element={<Intro />} />
+        <Route exact path="/" element={<Intro />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/admin" element={<AdminDashboard showToast={showToast} />} />
-        <Route path="/faculty" element={<FacultyDashboard showToast={showToast} />} />
+        <Route path="/admin" element={<AdminDashboard showToast={showAlert} />} />
+        <Route path="/faculty" element={<FacultyDashboard showToast={showAlert} />} />
         <Route path="/credits" element={<Credits />} />
-        <Route path="*" element={<Intro />} />
       </Routes>
-    </div>
+    </>
   );
 }
 
