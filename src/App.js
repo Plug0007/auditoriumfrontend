ed1442f35a5f173cb97dce2ce735a01bdca6ef89
Raelyaan plug <plug9999999@gmail.com>
2025-03-12 01:17:15 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
+// frontend/src/App.js
 import React, { useState } from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Header from './components/Header';
 import Intro from './components/Intro';
 import Home from './components/Home';
@@ -9,6 +10,20 @@ import FacultyDashboard from './components/FacultyDashboard';
 import Credits from './components/Credits';
 import AlertModal from './components/AlertModal';
 import './styles/App.css';
+
+/**
+ * PrivateRoute:
+ * Checks if a user exists in localStorage.
+ * If no user, redirects to /login (replace: true).
+ * If a user is found, renders the children.
+ */
+function PrivateRoute({ children }) {
+  const user = JSON.parse(localStorage.getItem('user'));
+  if (!user) {
+    return <Navigate to="/login" replace />;
+  }
+  return children;
+}
 
 function App() {
   const [alertMessage, setAlertMessage] = useState('');
@@ -23,15 +38,37 @@ function App() {
     <>
       <Header />
       {alertMessage && (
-        <AlertModal message={alertMessage} type={alertType} onClose={() => setAlertMessage('')} />
+        <AlertModal
+          message={alertMessage}
+          type={alertType}
+          onClose={() => setAlertMessage('')}
+        />
       )}
+
       <Routes>
+        {/* Public Routes */}
         <Route path="/" element={<Intro />} />
         <Route path="/home" element={<Home />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/admin" element={<AdminDashboard showToast={showAlert} />} />
-        <Route path="/faculty" element={<FacultyDashboard showToast={showAlert} />} />
         <Route path="/credits" element={<Credits />} />
+
+        {/* Protected Routes */}
+        <Route
+          path="/admin"
+          element={
+            <PrivateRoute>
+              <AdminDashboard showToast={showAlert} />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/faculty"
+          element={
+            <PrivateRoute>
+              <FacultyDashboard showToast={showAlert} />
+            </PrivateRoute>
+          }
+        />
       </Routes>
     </>
   );
